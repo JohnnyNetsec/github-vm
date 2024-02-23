028eed75d723c40cc68737c0b07f369757be3e6d
John Yan <116501930+JohnnyNetsec@users.noreply.github.com>
2024-02-23 09:22:40 -0500

    Create login.sh

---
+++ mac/login.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+echo ..........................................................
+echo ..........................................................
+echo IP:
+curl -s http://localhost:4040/api/tunnels | grep -o '"public_url":"[^"]*' | sed 's/"public_url":"//'
+echo Username: TheDisala
+echo Password: disalardp
